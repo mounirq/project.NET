2548b202bd12dcb5febee1a81d154ca7e21852c1
TidaneY <tidane.yasmine@gmail.com>
2018-09-04 14:17:56 +0200

    Settings de lappli

---
+++ ProjetNet/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Configuration;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+
+namespace ProjetNet
+{
+    internal class MainWindowViewModel : BindableBase
+    {
+        #region Private Fields
+
+        //public ObservableCollection<AbstractDataViewModel> ComponentInfoList { get; private set; }
+
+        #endregion Private Fields
+    }
+}
