29a780506cb95d9e9a2d5f1c70638d9750851b63
mounirq <33689859+mounirq@users.noreply.github.com>
2018-09-07 14:32:47 +0200

    Modification Hedgin tools

--- ProjetNet/Models/HedgingTool.cs
+++ ProjetNet/Models/HedgingTool.cs
@@ -1,4 +1,5 @@
 ﻿using PricingLibrary.FinancialProducts;
+using PricingLibrary.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,50 +12,62 @@ namespace ProjetNet.Models
     {
         #region Private Fields
 
-        private IOption _option;
-        private DateTime _startDateTest;
-        private IDataProvider _dataType;
-        private int _estimationStep;
+        private UserInput userInput;
+        private double[] optionValue;
+        private double[] portfolioValue;
+        private double normalizedGain;
 
         #endregion Private Fields
 
         #region Public Constructors
 
-        public HedgingTool(IOption option, DateTime startDateTest, IDataProvider dataType, int estimationStep)
+        public HedgingTool(UserInput userInput)
         {
-            _option = option;
-            _startDateTest = startDateTest;
-            _dataType = dataType;
-            _estimationStep = estimationStep;
+            this.userInput = userInput;
+            int numberOfRebalancing = (int) DayCount.CountBusinessDays(userInput.StartDate, userInput.Maturity)/ userInput.RebalancementFrequency;
+
+            this.optionValue = new double[numberOfRebalancing];
+            this.portfolioValue = new double[numberOfRebalancing];
+        }
+        #endregion Public Constructors
+
+        #region Public Methods
+
+        public IOption constructOption()
+        {
+            IOption option = null;
+
+            if (!(userInput.Weights == null) && userInput.Weights.Sum() != 1){throw new ArgumentException("The sum of the weights must equal");}
+            List<Share> underlyingsShares = new List<Share>();
+            foreach (string underlyingId in userInput.UnderlyingsIds)
+            {
+                String underlyingName = ShareName.GetShareName(underlyingId);
+                underlyingsShares.Add(new Share(underlyingName, underlyingId));
+            }
+            if (userInput.OptionType.Equals("VanillaCall"))
+            {
+                if(underlyingsShares.Count > 1) { throw new ArgumentException("You have to choose only one option "); }
+                option = new VanillaCall(userInput.OptionType, underlyingsShares[0], userInput.Maturity, userInput.Strike);
+            }
+            else if (userInput.OptionType.Equals("BasketOption"))
+            {
+                option = new BasketOption(userInput.OptionType, underlyingsShares.ToArray(), userInput.Weights, userInput.Maturity, userInput.Strike);
+            }
+            else
+            {
+                throw new ArgumentException("Unkown OptionType " + userInput.OptionType);
+            }
+            return option;
         }
 
-        #endregion Public Constructors
+        #endregion Public Methods
 
         #region Public Properties
 
-        public IOption Option
-        {
-            get { return _option; }
-            set { _option = value; }
-        }
-
-        public DateTime StartDateTest
-        {
-            get { return _startDateTest; }
-            set { _startDateTest = value; }
-        }
-
-        public IDataProvider DataType
-        {
-            get { return _dataType; }
-            set { _dataType = value; }
-        }
-
-        public int EstimationStep
-        {
-            get { return _estimationStep; }
-            set { _estimationStep = value; }
-        }
+        internal UserInput UserInput { get => userInput; set => userInput = value; }
+        public double[] OptionValue { get => optionValue; set => optionValue = value; }
+        public double[] PortfolioValue { get => portfolioValue; set => portfolioValue = value; }
+        public double NormalizedGain { get => normalizedGain; set => normalizedGain = value; }
 
         #endregion Public Properties
     }
